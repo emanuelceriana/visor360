a6a292c002d18e8a4ecec86bb138dac896b29faf
Emanuel Ceriana <emanuelceriana@gmail.com>
2019-01-09 16:03:12 -0200

    add path

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@ var div = document.getElementById('container');
 var PSV = new PhotoSphereViewer({
 	panorama: 'https://visor360.herokuapp.com/img/canon.jpg',
 	container: div,
-	loading_img: 'https://raw.githubusercontent.com/mistic100/Photo-Sphere-Viewer/3.1.0/example/photosphere-logo.gif',
 	navbar: 'autorotate zoom download fullscreen',
 	caption: 'Dique El Cadillal <b>&copy; Federico Silva</b>',
 	default_fov: 65,
