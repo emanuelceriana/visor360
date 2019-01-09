f567b07c348b174887a2aaba991af7b9bcaa49bc
Emanuel Ceriana <emanuelceriana@gmail.com>
2019-01-09 15:55:17 -0200

    add path

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 var div = document.getElementById('container');
-			var PSV = new PhotoSphereViewer({
-					panorama: 'http://tassedecafe.org/wp-content/uploads/2013/01/parc-saint-pierre-amiens.jpg',
-					container: div,
-          time_anim: 3000,
-          loading_img: 'https://raw.githubusercontent.com/mistic100/Photo-Sphere-Viewer/3.1.0/example/photosphere-logo.gif',
-					navbar: false,
-					navbar_style: {
-						backgroundColor: 'rgba(58, 67, 77, 0.7)'
-					},
-				});
+var PSV = new PhotoSphereViewer({
+	panorama: 'https://visor360.herokuapp.com/img/canon.jpg',
+	container: div,
+	loading_img: 'https://raw.githubusercontent.com/mistic100/Photo-Sphere-Viewer/3.1.0/example/photosphere-logo.gif',
+	navbar: 'autorotate zoom download fullscreen',
+	caption: 'Dique El Cadillal <b>&copy; Federico Silva</b>',
+	default_fov: 65,
+	mousewheel: false,
+
+});
