281d7e746939ca89a48459bc8deac89757c37042
Emanuel Ceriana <emanuelceriana@gmail.com>
2019-01-09 16:07:15 -0200

    add path

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ var div = document.getElementById('container');
 var PSV = new PhotoSphereViewer({
 	panorama: 'https://visor360.herokuapp.com/img/canon.jpg',
 	container: div,
+	loading_txt: 'Se está cargando la imágen 360 en alta calidad para una mejor experiencia, esto podria tardar varios segundos...',
 	navbar: 'autorotate zoom download fullscreen',
 	caption: 'Dique El Cadillal <b>&copy; Federico Silva</b>',
 	default_fov: 65,
