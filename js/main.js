b30e771efbb06c16271030ecee33f253603e2992
Emanuel Ceriana <emanuelceriana@gmail.com>
2019-01-09 22:59:50 -0200

    final

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ var div = document.getElementById('container');
 var PSV = new PhotoSphereViewer({
 	panorama: 'https://visor360.herokuapp.com/img/cadillal2.jpg',
 	container: div,
-	loading_img: 'https://visor360.herokuapp.com/img/loading.jpg',
+	loading_img: 'https://visor360.herokuapp.com/img/loading.gif',
 	navbar: 'autorotate zoom download fullscreen',
 	caption: 'Dique El Cadillal <b>&copy; Federico Silva</b>',
 	default_fov: 65,
